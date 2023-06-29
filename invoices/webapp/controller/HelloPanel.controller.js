3bcbc2972086045ab1c5b28d033bf9c5990f7eba
Jose Duarte <duartematiasduarte@gmail.com>
2023-06-29 10:11:22 +0000

    commit con el dialogo funcionando desde el contolador

--- invoices/webapp/controller/HelloPanel.controller.js
+++ invoices/webapp/controller/HelloPanel.controller.js
@@ -1,33 +1,49 @@
 sap.ui.define([
     'sap/ui/core/mvc/Controller',
-    'sap/m/MessageToast'
- 
-    
-], function(Controller,MessageToast) {
+    'sap/m/MessageToast',
+    'sap/ui/core/Fragment'
+           
+], 
+
+function(Controller, MessageToast,Fragment ) {
+
     'use strict';
 
-    
-       
-    function onShowHello(){
-        let oBundle = this.getView().getModel("i18n").getResourceBundle();
-        let sRecipient=this.getView().getModel().getProperty("/recipient/name");
-        var sMsg = oBundle.getText("helloMsg",[sRecipient])
-        MessageToast.show(sMsg );
-
-    }
-
-    function onOpenDialog(){
-        this.getOwnerComponent().openHelloDialog()
-
-    }
-
-    
+   
     return Controller.extend("logaligroup.invoices.controller.HelloPanel",{
 
-        onShowHello :onShowHello,
-        onOpenDialog:onOpenDialog
-        
-       
-    })
+        onInit: function() {
+                             
+        },
+
+        onShowHello : function () {
+            var oBundle = this.getView().getModel("i18n").getResourceBundle();
+            var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            var sMsg = oBundle.getText("helloMsg",[sRecipient]);
+            MessageToast.show(sMsg);
+           
+         },
+         onOpenDialog: function(){
+            const oView = this.getView();
+            if (!oView.byId("helloDialog")){
+                Fragment.load({
+                    id: oView.getId(),
+                    name: "logaligroup.invoices.view.HelloDialog",
+                    controller:this
+                }).then(function(oDialog){
+                    oView.addDependent(oDialog);
+                    oDialog.open();
+                });
+            }else {
+              this.byId("helloDialog").open();
+            }
+           
+         },
+     onCloseDialog : function(){
+        this.byId("helloDialog").close();
+
+     }
+    });
+
     
 }); 
