51060e9b4a3573816ba02ca23ac7f946cf42563b
Jose Duarte <duartematiasduarte@gmail.com>
2023-07-02 13:15:32 +0000

    10mo commit con las facturas ordenadas por(ShipperName) y clasificadas

--- invoices/webapp/controller/InvoicesList.controller.js
+++ invoices/webapp/controller/InvoicesList.controller.js
@@ -1,15 +1,35 @@
 sap.ui.define([
     'sap/ui/core/mvc/Controller',
-    'sap/ui/model/json/JSONModel'
-], function(Controller,JSONModel) {
+    'sap/ui/model/json/JSONModel',
+    'logaligroup/invoices/model/InvoicesFormatter',
+    'sap/ui/model/Filter',
+    'sap/ui/model/FilterOperator'
+], function(Controller,JSONModel,InvoicesFormatter,Filter,FilterOperator) {
     'use strict';
     return Controller.extend("logaligroup.invoices.controller.InvoicesList",{
+      formatter :InvoicesFormatter,
      onInit :  function(){
         let oViewModel = new JSONModel({
             usd:"USD",
             eur:"EUR"       
          });
         this.getView().setModel(oViewModel, "currency");     
+     },
+
+     onFilterInvoices : function(oEvent){
+
+        const aFilter=[];
+        const sQuery = oEvent.getParameter("query");
+        if (sQuery) {
+             aFilter.push(new Filter("ProducName",FilterOperator.Contains, sQuery));
+        };
+
+        const oList=this.getView().byId("InvoiceList");
+        const oBinding=oList.getBinding("items");
+        oBinding.filter(aFilter);
+        
      }
-    });
-});  
+    
+   
+});
+}); 
