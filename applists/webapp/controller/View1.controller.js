85bc08407efbbb7371260182a3b02302d5783cb7
Jose Duarte <duartematiasduarte@gmail.com>
2023-07-09 11:15:14 +0000

    5to commit con multiselect y mostrado datos de la columna selecionada

--- applists/webapp/controller/View1.controller.js
+++ applists/webapp/controller/View1.controller.js
@@ -7,12 +7,42 @@ sap.ui.define([
     function (Controller) {
         "use strict";
 
+        function getGroupHeader(oGroup){
+            var groupHeaderListItem =new sap.m.GroupHeaderListItem({
+                title: oGroup.key,
+                upperCase :true
+            })
+            return groupHeaderListItem;
+        }
+
+        function onSelectedRows(oEvent){
+            var standartListItem = this.getView().byId("standardList")
+            var selectedItems = standartListItem.getSelectedItems();
+
+            var i18nModel= this.getView().getModel("i18n").getResourceBundle();
+
+            if(selectedItems.length === 0){
+                sap.m.MessageToast.show(i18nModel.getText("noSelection"))
+             } else{
+                var textMessage = i18nModel.getText("selection")
+                for( var i in selectedItems){
+                var context =    selectedItems[i].getBindingContext();
+                var oContext = context.getObject();
+                textMessage=textMessage + "-" + oContext.Material;
+               
+                }
+                sap.m.MessageToast.show(textMessage)
+              }
+            }
+
         return Controller.extend("logaligroup.applists.controller.View1", {
             onInit: function () {
                 var oJsonModel = new sap.ui.model.json.JSONModel();
                 oJsonModel.loadData("./localService/mockdata/ListData.json");
                 this.getView().setModel(oJsonModel);
 
-            }
+            },
+            getGroupHeader:getGroupHeader,
+            onSelectedRows:onSelectedRows
         });
     });
