cc474b9ccf2c1b14a518cd86c748c6a467284bf0
Jose Duarte <duartematiasduarte@gmail.com>
2023-07-19 08:17:14 +0000

    7ma pantalla programa con dos modelos (ciudad y empleados  )

--- employees/webapp/controller/Main.controller.js
+++ employees/webapp/controller/Main.controller.js
@@ -28,7 +28,7 @@ sap.ui.define([
           }
 
           function onFilter(){
-            var oJson = this.getView().getModel().getData();
+            var oJson = this.getView().getModel("jsonCountries").getData();
             var filters = [];
             if(oJson.employeeId !== ""){
               filters.push(new sap.ui.model.Filter("EmployeeID", "EQ" ,oJson.employeeId))
@@ -41,14 +41,14 @@ sap.ui.define([
           oBinding.filter(filters);
          }
          function  onClearFilter() {
-          var oModel = this.getView().getModel();
+          var oModel = this.getView().getModel("jsonCountries");
           oModel.setProperty("/employeeId","")
           oModel.setProperty("/countryKey","")
          } 
          
          function  showPostalCode(oEvent){
            var itemPressed = oEvent.getSource();
-           var context = itemPressed.getBindingContext();
+           var context = itemPressed.getBindingContext("jsonEmployees");
            var object  =  context.getObject() ;
            sap.m.MessageToast.show(object.PostalCode);
 
@@ -57,32 +57,19 @@ sap.ui.define([
         return Controller.extend("logaligroup.employees.controller.Main", {
 
             onAfterRendering: function () {
-            var oJSONModel = new sap.ui.model.json.JSONModel();
-            var oView = this.getView();
+             var oView = this.getView();
             var i18nBundle = oView.getModel("i18n").getResourceBundle();
-          /*  var oJSON = {
-                employeeId : "123456",
-                countryKey: "UK",
-                listCountry: [
-                  {
-                    key:"US",
-                    text:i18nBundle.getText("countryUS")
-                  },
-                  {
-                    key:"UK",
-                    text:i18nBundle.getText("countryUS")
-                  },
-                  {
-                    key:"ES",
-                    text:i18nBundle.getText("countryUS")
-                  }      
-                 
-
-                ] };*/
-                
+           
+            
+            
+          
+            var oJSONModel = new sap.ui.model.json.JSONModel();
                 oJSONModel.loadData("./localService/mockdata/Employees.json",false);
-                oView.setModel(oJSONModel);
+                oView.setModel(oJSONModel,"jsonEmployees");
 
+                var oJSONModelCountries = new sap.ui.model.json.JSONModel();
+                oJSONModelCountries.loadData("./localService/mockdata/Countries.json",false);
+                oView.setModel(oJSONModelCountries,"jsonCountries");
             },
             onValidate:onValidate,
             onFilter:onFilter,
