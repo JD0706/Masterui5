cc9a1e021ddf9733126089542f6421878b95fc1d
Jose Duarte <duartematiasduarte@gmail.com>
2023-07-22 09:41:19 +0000

    Aplicacion Modificada a FlexibleCumnLayout mostrado 2 colunnas

--- employees/webapp/controller/Main.controller.js
+++ employees/webapp/controller/Main.controller.js
@@ -190,33 +190,7 @@ sap.ui.define([
 
         return Controller.extend("logaligroup.employees.controller.Main", {
 
-            onAfterRendering: function () {
-             var oView = this.getView();
-             var i18nBundle = oView.getModel("i18n").getResourceBundle();
-           
             
-            
-          
-             var oJSONModel = new sap.ui.model.json.JSONModel();
-                oJSONModel.loadData("./localService/mockdata/Employees.json",false);
-                oView.setModel(oJSONModel,"jsonEmployees");
-
-                var oJSONModelCountries = new sap.ui.model.json.JSONModel();
-                oJSONModelCountries.loadData("./localService/mockdata/Countries.json",false);
-                oView.setModel(oJSONModelCountries,"jsonCountries");
-
-               //Modelo para configurar la aplicacion 
-                var oJsonModelConfig = new sap.ui.model.json.JSONModel({
-
-                    visibleID :true,
-                    visibleName :true,
-                    visibleCity :false,
-                    visibleBtnShowCity :true,
-                    visibleBtnHideCity :false
-                          
-                  })
-                  oView.setModel(oJsonModelConfig,"jsonModelConfig");
-            },
             onValidate:onValidate,
             onFilter:onFilter,
             onClearFilter ,
